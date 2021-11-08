998e682b8ea93cd267a8246e6d3f8ffd965f320a
Abhilash Gokhale <68489919+AbhilashGokhale@users.noreply.github.com>
2021-11-08 15:14:01 +0530

    Update and rename Career.html to Career.php

--- Career.php
+++ Career.php
@@ -121,18 +121,77 @@
         <input type="file" name="example1"/>
     </p>
 
-    <!-- multiple input fields for the same input name, use brackets -->
-    <p>
-        <label>Add files (up to 2): </label><br/>
-        <input type="file" name="example2[]"/><br/>
-        <input type="file" name="example2[]"/>
-    </p>
+   <?php
 
-    <!-- one file input field that allows multiple files to be uploaded, use brackets -->
-    <p>
-        <label>Add files (multiple): </label><br/>
-        <input type="file" name="example3[]" multiple="multiple"/>
-    </p>
+use DI\ContainerBuilder;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Message\UploadedFileInterface;
+use Slim\Factory\AppFactory;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+$containerBuilder = new ContainerBuilder();
+$container = $containerBuilder->build();
+
+$container->set('upload_directory', __DIR__ . '/uploads');
+
+AppFactory::setContainer($container);
+$app = AppFactory::create();
+
+$app->post('/', function (ServerRequestInterface $request, ResponseInterface $response) {
+    $directory = $this->get('upload_directory');
+    $uploadedFiles = $request->getUploadedFiles();
+
+    // handle single input with single file upload
+    $uploadedFile = $uploadedFiles['example1'];
+    if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
+        $filename = moveUploadedFile($directory, $uploadedFile);
+        $response->getBody()->write('Uploaded: ' . $filename . '<br/>');
+    }
+
+    // handle multiple inputs with the same key
+    foreach ($uploadedFiles['example2'] as $uploadedFile) {
+        if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
+            $filename = moveUploadedFile($directory, $uploadedFile);
+            $response->getBody()->write('Uploaded: ' . $filename . '<br/>');
+        }
+    }
+
+    // handle single input with multiple file uploads
+    foreach ($uploadedFiles['example3'] as $uploadedFile) {
+        if ($uploadedFile->getError() === UPLOAD_ERR_OK) {
+            $filename = moveUploadedFile($directory, $uploadedFile);
+            $response->getBody()->write('Uploaded: ' . $filename . '<br/>');
+        }
+    }
+
+    return $response;
+});
+
+/**
+ * Moves the uploaded file to the upload directory and assigns it a unique name
+ * to avoid overwriting an existing uploaded file.
+ *
+ * @param string $directory The directory to which the file is moved
+ * @param UploadedFileInterface $uploadedFile The file uploaded file to move
+ *
+ * @return string The filename of moved file
+ */
+function moveUploadedFile(string $directory, UploadedFileInterface $uploadedFile)
+{
+    $extension = pathinfo($uploadedFile->getClientFilename(), PATHINFO_EXTENSION);
+
+    // see http://php.net/manual/en/function.random-bytes.php
+    $basename = bin2hex(random_bytes(8));
+    $filename = sprintf('%s.%0.8s', $basename, $extension);
+
+    $uploadedFile->moveTo($directory . DIRECTORY_SEPARATOR . $filename);
+
+    return $filename;
+}
+
+$app->run();?>
 
     <p>
         <input type="submit"/>
